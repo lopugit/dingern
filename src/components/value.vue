5ebba3efb2dddb6451bd9cd0f2e059e9c7cd3b83
lopugit <ireply@myleisure.com.au>
2020-07-06 11:35:02 +1000

    A lot of work on value component, only uses a string path to access value from store, helps with reactivity to not pass value directly as property

--- src/components/value.vue
+++ src/components/value.vue
@@ -1,229 +1,205 @@
 <template lang="pug">
 
-	//- :id=`isRoot() ? 'root' : ''`
-	.value(
-	)
-		style {{ value.style }}
-		//- template(v-for=`edge in edges`)
-		.editable-props-container.props-container
-			.editable-props-title editable props
-			.editable-props.props-list
-				//- edge( :editable-props=`{ value: _watchers }`)
-				template(v-for=`edge in editableProps`)
-					edge(
-							:props=`{
-								edge: {
-									parent: {
-										meta,
-										value,
-									},
-									edgeName: edge
-								},
-							}`
-						)
-		.own-props-container.props-container
-			.own-props-title own props
-			.own-props.props-list
-				//- edge( :props=`{ value: _watchers }`)
-				template(v-for=`edge in ownProps`)
-					edge(
-							:props=`{
-								edge: {
-									parent: {
-										meta,
-										value,
-									},
-									edgeName: edge
-								},
-							}`
-						)
-		.prototype-props-container.props-container
-			.prototype-props-title prototype props
-			.prototype-props.props-list
-				template(v-for=`edge in prototypeProps`)
-					edge(
-							:props=`{
-								edge: {
-									parent: {
-										meta,
-										value,
-									},
-									edgeName: edge
-								},
-							}`
-						)
+	.value-container
+		.value(
+			v-if=`basic`
+			contenteditable
+			@input=`updateValue`
+			ref="valueInput"
+		)
+		.value(
+			v-else
+		)
+			template(
+				v-for=`val,key in value`
+				)
+				edge(
+						:properties=`{
+							path: getsmart(thing, 'path', undefined)+epp(key)
+						}`
+					)
+			.add-edge(
+				@click=`addEdge`
+				contenteditable
+			) 
+				q-icon(
+					name="fas fa-plus"
+					size="8px"
+				)
+				//- @newValue=`newVal => setsmart(thing, 'value'+epp(key), newVal)`
 </template>
 
 <script>
-
-import edge from './edge.vue'
 
 export default {
 	name: 'value',
 	data(){
 		return {
 			vue: {
-				shallow: true,
-				props: [
-					"template",
-					"$data",
-					"methods",
-					"edgeName",
-				]
+				shallow: false,
 			},
-			style
-		}
-	},
-	computed: {
-		value: {
-			get(){
-				let val = this
-				let ret = {}
-				if(!this.getsmart(this, 'props.value', false)){
-					val = this
-					// val = this.gosmart(this, '$store.state.graph.root', this)
-				} else {
-					val = this.gosmart(this, 'props.value', { undefined: true })
-				}
-
-				// for(var prop in val){
-				// 	if(this.getsmart(this, 'vue.props', []).indexOf(prop) >= 0){
-				// 		ret[prop] = val[prop]
-				// 	}
-				// }
-				return val
-			}
-		},
-		editableProps: {
-			get(){
-				let ret = []
-				for(let key in this.value) {
-					if(this.vue.props.indexOf(key) > -1) ret.push(key)
-					// if(this.value.hasOwnProperty(key)) ret.push(key)
-					// if(!this.value.hasOwnProperty(key)) ret.push(key)
-				}
-
-				return ret
-			}
-		},
-		ownProps: {
-			get(){
-				let ret = []
-				for(let key in this.value) {
-					// if(this.vue.props.indexOf(key) > -1) ret.push(key)
-					if(this.value.hasOwnProperty(key)) ret.push(key)
-					// if(!this.value.hasOwnProperty(key)) ret.push(key)
-				}
-
-				return ret
-			}
-		},
-		prototypeProps: {
-			get(){
-				let ret = []
-				for(let key in this.value) {
-					// if(this.vue.props.indexOf(key) > -1) ret.push(key)
-					// if(this.value.hasOwnProperty(key)) ret.push(key)
-					if(!this.value.hasOwnProperty(key)) ret.push(key)
-				}
-
-				return ret
-			}
-		},
-		edges: {
-			get(){
-				let ret = {
-					ownProps: this.ownProps,
-					prototypeProps: this.prototypeProps,
-					editableProps: this.editableProps
-				}
-				return ret
-			}
+			thing: {},
+			cachedValue: null
 		}
 	},
 	created(){
 		globalThis.$store = this.$store
 		this.ai()
 	},
-	methods: {
-		isRoot(args){
-			let ret = !this.gosmart(this, 'meta.parent', undefined) && this.gosmart(this, 'value', this) == undefined
-			if(ret) this.setsmart(this, 'meta.root', true)
-			return this.gosmart(this, 'meta.root', false)
-		},
-		ai(args){
-			try {
-				if(this.isRoot()){
-					this.gosmart(globalThis, 'v', this)
-				}
-
-				this.propsHandler(this.props)
-				
-			} catch(err){console.error(err)}
-		},
-		propsHandler(n){
-			for(let key in n){
-				// this.setsmart(this, key, n[key])
+	computed: {
+		graph: {
+			get(){
+				return this.$store.state.graph.thing
 			}
 		},
-		mergeStrategy(source){
-			return source
+		value: {
+			get(){
+				let ret = this.getsmart(this.graph, this.thing.path, undefined)
+				return ret
+			}
+		},
+		basic: {
+			get(){
+				return !(typeof this.value == 'object' || typeof this.value == 'array') || (this.value === null)
+			}
+		},
+		isRoot: {
+			get(){
+				let ret = this.getsmart(this, 'properties.root', false)
+				return ret == true
+			}
+		},
+		prototypeProperties: {
+			get(){
+				let ret = []
+				if(typeof this.value == 'function'){
+					let tmp = { value: this.value }
+					for(let key of Object.getOwnPropertyNames(tmp.prototype)) {
+						ret.push(key)
+					}
+				} else {
+					for(let key of Object.getOwnPropertyNames(Object.getPrototypeOf(this.value))) {
+						ret.push(key)
+					}
+				}
+				return ret
+			}
+		},
+		localPath: {
+			get(){
+				let pathArray = this.pathArray
+				return pathArray[pathArray.length-1]
+			}
+		},
+		pathArray: {
+			get(){
+				return this.parsePropertyPath(this.thing.path)
+			}
 		}
 	},
+	methods: {
+		updateValue(e){
+			this.setsmart(this.graph, this.thing.path, e.target.innerText)
+			// this.$emit('newValue', e.target.innerText)
+		},
+		addEdge(args){
+			let edges = Object.keys(this.value).filter((value)=>value.indexOf("new edge") >= 0)
+			let newEdge = "new edge"
+			let i = 2
+			while(edges.indexOf(newEdge) >= 0){
+				newEdge = "new edge "+i
+				i++
+			}
+			this.setsmart(this.graph, this.thing.path+this.epp(newEdge), undefined)
+		},
+		ai(args){
+			// this should only run once 
+			this.initialize()
+
+			// these will run conditionally and possibly more than once
+			this.reloadCss()
+			this.renderValue()
+
+		},
+		initialize(){
+			if(!this.getsmart(this.thing, 'initialized', false)){
+				try {
+					this.merge(this.thing, this.properties, { clone: false })
+				} catch(err){console.error(err)}
+				try {
+					if(this.isRoot){
+						window.rootVue = this
+						this.gosmart(globalThis, 'v', this)
+					}
+				} catch(err){console.error(err)}
+				this.setsmart(this.thing, 'initialized', true)
+			}
+		},
+		renderValue() {
+			if(this.basic){
+				let ret = this.value
+				if(typeof this.value === 'undefined') ret = 'undefined'
+				if(this.value === null) ret = 'null'
+				// if(!this.equal(this.cachedValue, ret)){
+				// }
+				let innerText = this.getsmart(this.$refs, 'valueInput.innerText', ret)
+				if(innerText != ret){
+					this.setsmart(this.$refs, 'valueInput.innerText', ret)
+				}
+			}
+			
+		},
+		// TO DO
+		// fix getsmart via array of paths
+		// investigate pointer not being persistent through state
+		reloadCss(){
+			if(this.localPath == "css"){
+				let newStylesheet = this.jss.createStyleSheet(this.value).attach()
+				window.test = newStylesheet
+				if(this.pathArray instanceof Array){
+					let parentPath = this.pathArray.slice(0,this.pathArray.length-1)
+					let oldStylesheet = this.getsmart(window.nrgraph, [...parentPath, "stylesheet"], undefined)
+					if(oldStylesheet){
+						this.jss.removeStyleSheet(oldStylesheet)
+					}
+					this.setsmart(
+						window.nrgraph, 
+						[...parentPath, "stylesheet"], 
+						newStylesheet
+					)
+				}
+			}
+		},
+	},
   watch: {
-		'props': {
+		'isRoot': {
+			handler: function(n,o){
+				if(n){
+					this.$native.window.rootVue = this
+				}
+			}
+		},
+		'value': {
+			deep: true,
+			handler: function(n,o){
+				this.ai()
+			}
+		},
+		'properties': {
 			handler: function(n,o){
 				if(!this.equal(n,o)){
-					this.propsHandler(n)
+					this.merge(this.thing, n, { clone: false })
+					// this.schema(this, n)
+					// this.propertiesHandler(n)
 				}
 			}
 		},
   },
 	props: {
-		props: {}
+		properties: {}
 	},
 	components: {
-		edge: edge
+		edge: () => import('./edge.vue')
 	}
 }
-
-let style = `
-	#root {
-		// background-color: rgb(0, 0, 0);
-		background-color: rgb(253, 253, 253);
-	}
-
-	.props-container {
-		font-weight: 500;
-		padding-left: 5px;
-	}
-
-	.props-list {
-		padding-left: 5px;
-	}
-
-	.editable-props-container {
-		color: rgb(66,185,131);
-	}
-
-	.editable-props {
-	}
-
-	.own-props-container {
-		color: rgb(249, 171, 1);
-	}
-
-	.own-props {
-	}
-	
-	.prototype-props-container {
-		color: rgb(26, 115, 232);
-	}
-
-	.prototype-props {
-	}
-
-
-`
-
 </script>
